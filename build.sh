5683a202456e246e7bd916aafd21e6c6b36717d1
smwms1 <smw@ylur.me>
2025-01-04 17:47:38 +0000

    Miscellaneous changes

--- build.sh
+++ build.sh
@@ -3,12 +3,5 @@
 BUILDDIR=builddir
 ROOTDIR=$(pwd)
 
-# Actually build ginitd
-if [ -z "${*##*-c*}" ]
-then
-	echo "Reconfiguring build system..."
-	meson setup $BUILDDIR
-fi
-
 echo "Compiling ginitd..."
 meson compile -C $BUILDDIR
