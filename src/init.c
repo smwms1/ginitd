7100e61e2241fcee9856e52aebee28099abdb308
smwms1 <smw@ylur.me>
2025-01-05 18:36:33 +0000

    Fix for new-style logging in init(8)

--- src/init.c
+++ src/init.c
@@ -1,11 +1,15 @@
 #include <unistd.h>
 
+#include "logging.h"
 #include "init_main.h"
 #include "init_userspace.h"
 
 int main(int argc, char **argv) {
-	if (getpid() != 1)
+	if (getpid() != 1) {
+		logging_init("init", LOGGING_FACIL_USER);
 		return init_userspace_main(argc, argv);
+	}
 
+	logging_init("init", LOGGING_FACIL_INIT);
 	return init_main(argc, argv);
 }
