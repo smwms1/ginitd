400c9e59fa4f705f4af208d1db61cf24cc77288a
smwms1 <smw@ylur.me>
2025-01-27 22:43:07 +0000

    Remove supervise module

--- src/supervise_control_mk.h
+++
@@ -1,6 +0,0 @@
-#ifndef GINITD_SUPERVISE_CONTROL_MK_H
-#define GINITD_SUPERVISE_CONTROL_MK_H
-
-int supervise_make_control(void);
-
-#endif /* GINITD_SUPERVISE_CONTROL_MK_H */
--- src/supervise_private.c
+++
@@ -1,8 +0,0 @@
-#include <limits.h>
-
-#include "supervise_private.h"
-
-char	supervise_services_path[PATH_MAX];
-char	supervise_tempdir_path[PATH_MAX];
-char	supervise_status_path[PATH_MAX];
-int		supervise_control;
